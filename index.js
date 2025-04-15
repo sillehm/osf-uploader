964997dca3900398ce55555e012ad63ffc34c2b1
sillehm <90754615+sillehm@users.noreply.github.com>
2025-04-15 08:25:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const ASSIGNMENT_FILE = path.join("scale_counts.csv");
 // Create file if not present
 function initCountsFile() {
   if (!fs.existsSync(ASSIGNMENT_FILE)) {
-    const initial = "condition,count\noriginal,9\nlabels,7\nlinear,5";
+    const initial = "condition,count\noriginal,0\nlabels,2\nlinear,5";
     fs.writeFileSync(ASSIGNMENT_FILE, initial);
   }
 }
